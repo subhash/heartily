636577c2a7bc29c366f5ba52d226d880294735c0
Subhash Gopalakrishnan <subhashgo@gmail.com>
2015-04-27 19:08:57 +0530

    Incorporating time

--- src/heartily/core.clj
+++ src/heartily/core.clj
@@ -1,7 +1,26 @@
 (ns heartily.core
   (:require [heartily.db :as hdb]
-            [heartily.gpx :as gpx]))
+            [heartily.gpx :as gpx]
+            [datomic.api :as d]))
 
 (defn store-activity [name gpxfile]
   (hdb/save-activity (gpx/gpx->map name gpxfile)))
 
+(store-activity "trek1" "resources/data/trek1.gpx")
+(store-activity "trek2" "resources/data/trek2.gpx")
+(store-activity "trek3" "resources/data/trek3.gpx")
+(store-activity "trek4" "resources/data/trek4.gpx")
+
+(defn heart-beats-for [activity]
+  (->> (d/q '[:find ?tm ?hr
+              :in $ ?n
+              :where
+              [?a :activity/name ?n]
+              [?a :activity/track ?t]
+              [?t :track/track-points ?tp]
+              [?tp :track-point/heart-rate ?hr]
+              [?tp :track-point/time ?tt]
+              [(.getTime ?tt) ?tm]]
+       (d/db hdb/conn) activity)
+       (sort-by first)
+       (map second)))
--- src/heartily/gpx.clj
+++ src/heartily/gpx.clj
@@ -2,7 +2,8 @@
   (:require [clojure.xml :as xml]
             [clojure.zip :refer [xml-zip down right node]]
             [clojure.data.zip.xml :refer [xml-> xml1-> attr text]]
-            [datomic.api :as d]))
+            [datomic.api :as d]
+            [clojure.instant]))
 
 
 (defn track-point->map [z]
@@ -12,6 +13,7 @@
             :track-point/heart-rate (xml1-> z :extensions (keyword "gpxtpx:TrackPointExtension") (keyword "gpxtpx:hr") text #(Integer/valueOf %))
             :track-point/speed (xml1-> z :extensions (keyword "gpxdata:speed") text #(Float/valueOf %))
             :track-point/vertical-speed (xml1-> z :extensions (keyword "gpxdata:verticalSpeed") text #(Float/valueOf %))
+            :track-point/time (xml1-> z :time text #(clojure.instant/read-instant-date %))
             :db/id (d/tempid :db.part/user)}]
     (->> tp (filter val) (into {}))))
 
