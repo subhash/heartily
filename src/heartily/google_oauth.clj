6b6bd6ffbd251075e639c5c4b59c039dffacc9d5
Subhash Gopalakrishnan <subhashgo@gmail.com>
2015-05-19 15:18:17 +0530

    OAuth2 and redirect works, data extracted

---
+++ src/heartily/google_oauth.clj
@@ -0,0 +1,34 @@
+(ns heartily.google-oauth
+  (:require [clj-oauth2.client :as oauth2]))
+
+(def login-uri "https://accounts.google.com")
+(def google-com-oauth2
+        {:authorization-uri (str login-uri "/o/oauth2/auth")
+         :access-token-uri (str login-uri "/o/oauth2/token")
+         :redirect-uri "http://localhost:3000/oauth2_callback"
+         :client-id "776188546157-5btudf4ic02tk5vsh6tdp72dpoi54b3v.apps.googleusercontent.com"
+         :client-secret "Fhf-_ms1QH43MOH1CkZcxvt3"
+         :access-query-param :access_token
+         :scope ["https://www.googleapis.com/auth/fitness.activity.write"]
+         :grant-type "authorization_code"
+         :access-type "offline"
+         :approval_prompt "force"})
+
+(def auth-req (oauth2/make-auth-request google-com-oauth2))
+
+(defn google-auth-uri []
+  (:uri auth-req))
+
+(defn google-get [uri access-token]
+  (let [response (oauth2/get uri {:oauth2 access-token})]
+    response))
+
+(defn google-access-token [params]
+  (println "access token")
+  (oauth2/get-access-token google-com-oauth2 params auth-req))
+
+
+
+;; Redirect them to (:uri auth-req)
+
+;; When they comeback to /authentication/callback
--- src/heartily/server.clj
+++ src/heartily/server.clj
@@ -1,32 +1,22 @@
 (ns heartily.server
   (:use compojure.core)
-  (:require [clj-oauth2.client :as oauth2]
-            [compojure.handler :as handler]
+  (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [heartily.views :as views]))
+            [heartily.views :as views]
+            [heartily.google-oauth :refer :all]
+            [ring.util.response :as resp]))
 
-;(def auth-url "https://accounts.google.com/o/oauth2/auth?redirect_uri=http://localhost:3000/code&response_type=code&client_id=776188546157-5btudf4ic02tk5vsh6tdp72dpoi54b3v.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Ffitness.activity.write&approval_prompt=force&access_type=offline")
-
-
-(defn auth-req []
-  (let [login-uri "https://accounts.google.com"
-        google-com-oauth2
-        {:authorization-uri (str login-uri "/o/oauth2/auth")
-         :access-token-uri (str login-uri "/o/oauth2/token")
-         :redirect-uri "http://localhost:3000/code"
-         :client-id "776188546157-5btudf4ic02tk5vsh6tdp72dpoi54b3v.apps.googleusercontent.com"
-         :client-secret "Fhf-_ms1QH43MOH1CkZcxvt3"
-         :access-query-param :access_token
-         :scope ["https://www.googleapis.com/auth/fitness.activity.write"]
-         :grant-type "authorization_code"
-         :access-type "offline"
-         :approval_prompt "force"}]
-    (oauth2/make-auth-request google-com-oauth2)))
-
+(defn save-token [req]
+  (let [foo (println "about to access-token")
+        access-token (google-access-token (:params req))
+        data (google-get "https://www.googleapis.com/fitness/v1/users/me/dataSources" access-token)]
+    (println "data - " data)
+    (resp/redirect "/done" )))
 
 (defroutes app-routes
-  (GET "/" [] (views/index-page (:uri (auth-req))))
-  (GET "/code" {params :params} (str params))
+  (GET "/" [] (views/index-page (google-auth-uri)))
+  (GET "/oauth2_callback" [] save-token)
+  (GET "/done" [] "done")
   (route/resources "/")
   (route/not-found "Not Found"))
 
